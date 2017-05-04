d36fc8081176d41ddb7cb0889bbec92c996f7425
adetoito <adetoito2@gmail.com>
2017-05-04 14:26:44 -0400

    Commit Dos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,26 @@ public class Main {
 	    Scanner sc = new Scanner(System.in);
         Scanner fileReader = new Scanner(new File("src/com/company/rawDataU6.txt"));
         int index = 0;
+        ArrayList<Student> students = new ArrayList();
+
         while (fileReader.hasNextLine()) {
             if (index != 0) {
                 String line = fileReader.nextLine();
-                
+                String [] divisions = line.split("\t");
+                int secretNumber = Integer.parseInt(divisions[0]);
+                if (students.size() > 0) {
+                    for (int i = 0; i < students.size(); i++) {
+                        if (secretNumber != students.get(i).returnSecretNum()) {
+                            Student s = new Student(secretNumber);
+                            students.add(s);
+                            break;
+                        }
+                    }
+                } else {
+                    Student s = new Student(secretNumber);
+                    students.add(s);
+                }
+
             }
             index++;
         }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -4,13 +4,18 @@ public class Student {
 
     private String name;
     private int secNum;
+    private double points;
 
     public Student (int sn) {
         secNum = sn;
     }
 
-    public void setName (String n) {
-        name = n;
-    }
+    public void setName (String n) { name = n; }
+
+    public int returnSecretNum () { return secNum; }
+
+    public void addPts (double pts) { points += pts; }
+
+    public void subtractPts (double pts) { points -= pts; }
 
 }
